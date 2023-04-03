0bd6d5429e7226491dc418d5e60993960ee96deb
ummataliyev <djohnummataliyev@gmail.com>
2023-04-03 17:05:03 +0500

    Preparing for deploy

--- new_project/settings.py
+++ new_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c_58p$f%5w31r=oyw&aaftn%el+8s4r2x3s!horprru6fg7udl' # noqa
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-mohidev-demo.uz", 'www.django-mohirdev0demo.uz', '127.0.0.1']
 
 
 # Application definition
